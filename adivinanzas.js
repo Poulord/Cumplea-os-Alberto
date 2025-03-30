985a290b4eb6c888b41c56731ebc2b34994b5720
Pablo <pablogo@msmk.university>
2025-03-30 10:41:29 +0200

    Implementación del  buscador para acceder a la pagina secreto.html, actualización de las adivinanzas en el archivo adivinanzas.js y organización nueva de la web

--- adivinanzas.js
+++ adivinanzas.js
@@ -7,9 +7,9 @@ const correctAnswers = {
 
 // Mensajes secretos que se mostrarán al acertar
 const secretMessages = {
-    1: "Alberto, eres el mejor portero que conozco. ¡Ataja todos los retos como atajas los balones!",
-    2: "Como buen conductor, sigues siempre el camino correcto en la vida. ¡Eres un amigo de los de verdad!",
-    3: "Hala Madrid! Eres tan leal a tus amigos como los madridistas a su equipo. ¡Eso no tiene precio!"
+    1: "Alberto, eres el mejor portero que conozco. ¡Ataja todos los retos como atajas los balones! por cierto.....(A)",
+    2: "Como buen conductor, sigues siempre el camino correcto en la vida. ¡Eres un amigo de los de verdad! Ya en nada!!! (V)",
+    3: "Hala Madrid! Eres tan leal a tus amigos como los madridistas a su equipo. ¡Eso no tiene precio! ¡¡¡La Última!!!  (20)"
 };
 
 function checkAnswer(riddleNumber) {
